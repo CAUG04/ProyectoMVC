38b6e03da18d379cf60abfa7161dd5dc67a65550
Carlos Urrego <carlos.urrego@tekus.co>
2017-04-25 19:25:02 -0500

    Se agrega eliminar, guardar y crud

--- Model/Alumno.cs
+++ Model/Alumno.cs
@@ -4,7 +4,9 @@ namespace Model
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity;
     using System.Data.Entity.Spatial;
+    using System.Linq;
 
     [Table("Alumno")]
     public partial class Alumno
@@ -39,6 +41,83 @@ namespace Model
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<AlumnoCurso> AlumnoCurso { get; set; }
 
+        public List<Alumno> Listar()
+        {
+            var alumnos = new List<Alumno>();
 
+            try
+            {
+                using (var ctx = new TetsContext())
+                {
+                    alumnos = ctx.Alumno.ToList();
+                }
+            }
+            catch (Exception exception)
+            {
+                throw;
+            }
+            return alumnos;
+        }
+
+        public Alumno Obtener(int id)
+        {
+            var alumno = new Alumno();
+
+            try
+            {
+                using (var ctx = new TetsContext())
+                {
+                    alumno = ctx.Alumno.Include("AlumnoCurso")
+                        .Include("AlumnoCurso.Curso")
+                        .Where(x => x.id == id)
+                        .SingleOrDefault();
+                }
+            }
+            catch (Exception exception)
+            {
+                throw;
+            }
+            return alumno;
+        }
+
+        public void Guardar()
+        {
+            try
+            {
+                using (var ctx = new TetsContext())
+                {
+                    if (this.id > 0)
+                    {
+                        ctx.Entry(this).State = EntityState.Modified;
+                    }
+                    else
+                    {
+                        ctx.Entry(this).State = EntityState.Added;
+                    }
+                    ctx.SaveChanges();
+                }
+            }
+            catch (Exception exception)
+            {
+                throw;
+            }
+        }
+
+        public void Eliminar()
+        {
+            try
+            {
+                using (var ctx = new TetsContext())
+                {
+
+                    ctx.Entry(this).State = EntityState.Deleted;
+                    ctx.SaveChanges();
+                }
+            }
+            catch (Exception exception)
+            {
+                throw;
+            }
+        }
     }
 }
