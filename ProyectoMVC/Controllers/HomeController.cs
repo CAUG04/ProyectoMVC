eae17166368d5e45525ac28176e4bc1bfd1c66ab
Carlos Urrego <carlos.urrego@tekus.co>
2017-04-26 19:57:43 -0500

    Se agrega la modalidad cursos y aun falta terminar

--- ProyectoMVC/Controllers/HomeController.cs
+++ ProyectoMVC/Controllers/HomeController.cs
@@ -10,6 +10,8 @@ namespace ProyectoMVC.Controllers
     public class HomeController : Controller
     {
         private Alumno alumno = new Alumno();
+        private AlumnoCurso alumno_curso = new AlumnoCurso();
+        private Curso curso = new Curso();
 
         // GET: Home
         public ActionResult Index()
@@ -23,7 +25,16 @@ namespace ProyectoMVC.Controllers
             return View(alumno.Obtener(id));
         }
 
-        //Home/Crud
+        //home/ver/?Alumno_id=1
+        public PartialViewResult Cursos(int Alumno_id)
+        {
+            ViewBag.Cursos = curso.Todos(Alumno_id);
+            alumno_curso.Alumno_id = Alumno_id;
+
+            return PartialView(alumno_curso);
+        }
+
+        //home/crud
         public ActionResult Crud(int id = 0)
         {
             return View(
@@ -33,18 +44,21 @@ namespace ProyectoMVC.Controllers
         }
 
         //Home/Guardar
-        public ActionResult Guardar(Alumno model)
+        public JsonResult Guardar(Alumno model)
         {
+            var rm = new ResponseModel();
+
             if (ModelState.IsValid)
             {
-                model.Guardar();
-                return Redirect("~/home");
+                rm = model.Guardar();
+
+                if (rm.response)
+                {
+                    rm.href = Url.Content("~/home");
+                }
             }
-            else
-            {
-                return View("~/views/home/crud.cshtml", model);
-            }
-           
+
+            return Json(rm);
         }
 
         //Home/Eliminar
